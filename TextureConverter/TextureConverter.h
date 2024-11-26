b193e0e29f9f861bd6870e32686684e91c1ecfb6
TsuchiyaTomonori <k022g0017@g.neec.ac.jp>
2024-11-26 13:11:09 +0900

    終わり

---
+++ TextureConverter/TextureConverter.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+
+class TextureConverter
+{
+public:
+
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void ConvertTextureWICToDDS(const std::string& filePath);
+	
+private:
+
+	/// <summary>
+	/// テクスチャファイル読み込み
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+private:
+
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換
+	/// </summary>
+	/// <param name="mString">マルチバイト文字列</param>
+	/// <returns>ワイド文字列</returns>
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+private:
+
+
+};
+
--- TextureConverter/main.cpp
+++ TextureConverter/main.cpp
@@ -1,16 +1,35 @@
+#include <Windows.h>
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
+#include "TextureConverter.h"
+
+
+//コマンドライン引数
+enum Argument {
+	kApplicationPath,   //アプリケーションのパス 
+	kFilePath,          //渡されたファイルのパス
+
+	NumArgument
+};
 
 int main(int argc, char* argv[]) {
 
-	for (int i = 0; i < argc; i++)
-	{
-		printf(argv[i]);
+	assert(argc >= NumArgument);
 
-		printf("\n");
-	}
+	// COM ライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	//テクスチャコンバータ
+	TextureConverter converter;
+
+	//テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	// COMライブラリの終了
+	CoUninitialize();
 
 	system("pause");
-	
 	return 0;
 }
