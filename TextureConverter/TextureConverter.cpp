d44255d0c1fb328578ab6c567767d2e373d502db
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2024-12-02 15:49:10 +0900

    ２－６終了？

--- TextureConverter/TextureConverter.cpp
+++ TextureConverter/TextureConverter.cpp
@@ -75,9 +75,29 @@ void TextureConverter::SeparateFilePath(const std::wstring& filePath)
 
 void TextureConverter::SaveDDSTextureToFile()
 {
-	metadata_.format = MakeSRGB(metadata_.format);
-
 	HRESULT result;
+
+	ScratchImage mipChain;
+	result = GenerateMipMaps(
+		scratchImage_.GetImages(), scratchImage_.GetImageCount(), scratchImage_.GetMetadata(),
+		TEX_FILTER_DEFAULT, 0, mipChain);
+	if (SUCCEEDED(result))
+	{
+		scratchImage_ = std::move(mipChain);
+		metadata_ = scratchImage_.GetMetadata();
+	}
+
+	ScratchImage converted;
+	result = Compress(scratchImage_.GetImages(), scratchImage_.GetImageCount(), metadata_,
+	DXGI_FORMAT_BC7_UNORM_SRGB, TEX_COMPRESS_BC7_QUICK | TEX_COMPRESS_SRGB_OUT |
+	TEX_COMPRESS_PARALLEL, 1.0f, converted);
+	if (SUCCEEDED(result))
+	{
+		scratchImage_ = std::move(converted);
+		metadata_ = scratchImage_.GetMetadata();
+	}
+
+	metadata_.format = MakeSRGB(metadata_.format);
 
 	std::wstring filePath = directoryPath_ + fileName_ + L".dds";
 
